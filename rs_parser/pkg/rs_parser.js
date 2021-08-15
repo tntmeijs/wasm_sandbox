caa312f019d1fb723a33e7fc78fbf96ddba96078
tntmeijs <tntmeijs@gmail.com>
2021-08-15 16:31:22 +0200

    Added generated WebAssembly files

---
+++ rs_parser/pkg/rs_parser.js
@@ -0,0 +1,69 @@
+
+let wasm;
+
+/**
+* @param {number} start_balance
+* @param {number} end_balance
+* @param {number} items_purchased
+* @param {number} outstanding_debt
+* @returns {number}
+*/
+export function parse(start_balance, end_balance, items_purchased, outstanding_debt) {
+    var ret = wasm.parse(start_balance, end_balance, items_purchased, outstanding_debt);
+    return ret >>> 0;
+}
+
+async function load(module, imports) {
+    if (typeof Response === 'function' && module instanceof Response) {
+        if (typeof WebAssembly.instantiateStreaming === 'function') {
+            try {
+                return await WebAssembly.instantiateStreaming(module, imports);
+
+            } catch (e) {
+                if (module.headers.get('Content-Type') != 'application/wasm') {
+                    console.warn("`WebAssembly.instantiateStreaming` failed because your server does not serve wasm with `application/wasm` MIME type. Falling back to `WebAssembly.instantiate` which is slower. Original error:\n", e);
+
+                } else {
+                    throw e;
+                }
+            }
+        }
+
+        const bytes = await module.arrayBuffer();
+        return await WebAssembly.instantiate(bytes, imports);
+
+    } else {
+        const instance = await WebAssembly.instantiate(module, imports);
+
+        if (instance instanceof WebAssembly.Instance) {
+            return { instance, module };
+
+        } else {
+            return instance;
+        }
+    }
+}
+
+async function init(input) {
+    if (typeof input === 'undefined') {
+        input = new URL('rs_parser_bg.wasm', import.meta.url);
+    }
+    const imports = {};
+
+
+    if (typeof input === 'string' || (typeof Request === 'function' && input instanceof Request) || (typeof URL === 'function' && input instanceof URL)) {
+        input = fetch(input);
+    }
+
+
+
+    const { instance, module } = await load(await input, imports);
+
+    wasm = instance.exports;
+    init.__wbindgen_wasm_module = module;
+
+    return wasm;
+}
+
+export default init;
+
